939c7353485316b78ddac61470560f6331f8d58b
Henry Lynx <simeonparvanov@ymail.com>
2015-05-02 11:36:07 +0200

    PSCS4 Week 01 first excersice. String manipulation with delegates

---
+++ ASSIGNM2_week1_START/StringManipulatingApp/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace StringManipulatingApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnToCapitals = new System.Windows.Forms.Button();
+            this.btnDashToUnderscore = new System.Windows.Forms.Button();
+            this.richTextBoxIn = new System.Windows.Forms.RichTextBox();
+            this.richTextBoxOut = new System.Windows.Forms.RichTextBox();
+            this.btnRemoveVowels = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnToCapitals
+            // 
+            this.btnToCapitals.Location = new System.Drawing.Point(246, 27);
+            this.btnToCapitals.Name = "btnToCapitals";
+            this.btnToCapitals.Size = new System.Drawing.Size(180, 23);
+            this.btnToCapitals.TabIndex = 2;
+            this.btnToCapitals.Text = "Change to capitals";
+            this.btnToCapitals.UseVisualStyleBackColor = true;
+            this.btnToCapitals.Click += new System.EventHandler(this.btnToCapitals_Click);
+            // 
+            // btnDashToUnderscore
+            // 
+            this.btnDashToUnderscore.Location = new System.Drawing.Point(246, 56);
+            this.btnDashToUnderscore.Name = "btnDashToUnderscore";
+            this.btnDashToUnderscore.Size = new System.Drawing.Size(180, 23);
+            this.btnDashToUnderscore.TabIndex = 3;
+            this.btnDashToUnderscore.Text = "Change Spaces to underscores";
+            this.btnDashToUnderscore.UseVisualStyleBackColor = true;
+            this.btnDashToUnderscore.Click += new System.EventHandler(this.btnDashToUnderscore_Click);
+            // 
+            // richTextBoxIn
+            // 
+            this.richTextBoxIn.Location = new System.Drawing.Point(46, 29);
+            this.richTextBoxIn.Name = "richTextBoxIn";
+            this.richTextBoxIn.Size = new System.Drawing.Size(100, 96);
+            this.richTextBoxIn.TabIndex = 4;
+            this.richTextBoxIn.Text = "";
+            // 
+            // richTextBoxOut
+            // 
+            this.richTextBoxOut.Location = new System.Drawing.Point(553, 27);
+            this.richTextBoxOut.Name = "richTextBoxOut";
+            this.richTextBoxOut.Size = new System.Drawing.Size(100, 96);
+            this.richTextBoxOut.TabIndex = 5;
+            this.richTextBoxOut.Text = "";
+            // 
+            // btnRemoveVowels
+            // 
+            this.btnRemoveVowels.Location = new System.Drawing.Point(246, 85);
+            this.btnRemoveVowels.Name = "btnRemoveVowels";
+            this.btnRemoveVowels.Size = new System.Drawing.Size(180, 23);
+            this.btnRemoveVowels.TabIndex = 6;
+            this.btnRemoveVowels.Text = "Remove Vowels";
+            this.btnRemoveVowels.UseVisualStyleBackColor = true;
+            this.btnRemoveVowels.Click += new System.EventHandler(this.btnRemoveVowels_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(246, 114);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(180, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "All First Letters ToUpper";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(708, 262);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnRemoveVowels);
+            this.Controls.Add(this.richTextBoxOut);
+            this.Controls.Add(this.richTextBoxIn);
+            this.Controls.Add(this.btnDashToUnderscore);
+            this.Controls.Add(this.btnToCapitals);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnToCapitals;
+        private System.Windows.Forms.Button btnDashToUnderscore;
+        private System.Windows.Forms.RichTextBox richTextBoxIn;
+        private System.Windows.Forms.RichTextBox richTextBoxOut;
+        private System.Windows.Forms.Button btnRemoveVowels;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ ASSIGNM2_week1_START/StringManipulatingApp/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+
+namespace StringManipulatingApp
+{
+    public partial class Form1 : Form
+    {
+        //delegate for the method change to capital
+        public delegate string StringChange(string input);
+        public StringChange stringChange;
+        //delegate for the second method dash to underscore
+        //public delegate string 
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public void Change(StringChange changeVariable)
+        {
+            stringChange = new StringChange(changeVariable);
+            richTextBoxOut.Text = stringChange(richTextBoxIn.Text);
+        }
+        public string ChangeToCapital(string input)
+        {
+            //char[] arr1 = str.ToCharArray(); or: string s = new string (arr1);
+            char[] arr1 = input.ToCharArray();
+            int l = arr1.Length;
+            string[] auxArr1 = new string[l];
+            for (int i = 0; i < arr1.Length; i++)
+			{
+                auxArr1[i] = arr1[i].ToString().ToUpper();
+			}
+            string capitalArr = string.Join("",auxArr1);
+            return capitalArr;
+        }
+        //public string RemoveVowels(string input)
+        //{
+        //    char[] arr1 = input.ToCharArray();
+        //    int l = arr1.Length;
+        //    string[] auxArr1 = new string[l];
+        //    for (int i = 0; i < arr1.Length; i++)
+        //    {
+        //        if (arr1[i] == 'a'|| arr1[i] == 'e' || arr1[i] =='o'||arr1[i] =='u'||arr1[i] == 'i'||arr1[i] =='y')
+        //        {
+        //            auxArr1[i] = "";
+        //        }
+        //        else
+        //        {
+        //            auxArr1[i] = arr1[i].ToString();
+        //        }
+        //    }
+        //    string stringArr = string.Join("", auxArr1);
+        //    return stringArr;
+        //}
+
+        /*
+         * this is a linq query that selects all the characters from a string where the character is 
+         * not contained in the vowels string (that is the Where part) - the ToArray() is putting all 
+         * these characters in an array and is necessary since one of the string constructor overloads 
+         * takes a character array as input (but doesn't work with an enumeration)
+         */
+        public string RemoveVowels(string input)
+        {
+            string vowels = "AaEeIiOoUuYy";
+            input = new string(input.Where(c => !vowels.Contains(c)).ToArray());
+            return input;
+        }
+
+        /*
+         * Method to uppercase first letters
+         */
+
+        public string UppercaseWords(string input)
+        {
+            char[] arr1 = input.ToCharArray();
+            //it the array is longer than 1
+            if (arr1.Length >= 1)
+            {
+                //if the first letter is lower case
+                if (char.IsLower(arr1[0]))
+                {
+                    //transform the first letter from lower to uppercase 
+                    arr1[0] = char.ToUpper(arr1[0]);
+                }
+            }
+            //Scan the letters, checking for spaces between the words
+            //if there is space check if the next letter is lower case and change it to upper case.
+            for (int i = 1; i < arr1.Length; i++)
+            {
+                if (arr1[i - 1] == ' ')
+                //if (arr1[i] == ' ')
+                {
+                    if (char.IsLower(arr1[i]))
+                    {
+                        arr1[i] = char.ToUpper(arr1[i]);
+                    }
+                }
+            }
+            return new string(arr1);
+        }
+        public string ChangeSpaceToUnderscore(string input)
+        {
+            return input.Replace('-', '_');
+        }
+
+        private void btnToCapitals_Click(object sender, EventArgs e)
+        {
+            Change(ChangeToCapital);
+        }
+        private void btnDashToUnderscore_Click(object sender, EventArgs e)
+        {
+            Change(ChangeSpaceToUnderscore);
+        }
+
+        private void btnRemoveVowels_Click(object sender, EventArgs e)
+        {
+            Change(RemoveVowels);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Change(UppercaseWords);
+        }
+    }
+}
